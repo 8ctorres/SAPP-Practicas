0e39b807d3a387b51b35d25c8abe7ebddc3bc344
Carlos Torres <carlos.torres@udc.es>
2022-11-08 14:26:30 +0100

    WIP: Deserialización insegura

--- src/main/java/es/storeapp/web/interceptors/AutoLoginInterceptor.java
+++ src/main/java/es/storeapp/web/interceptors/AutoLoginInterceptor.java
@@ -6,12 +6,22 @@ import es.storeapp.common.Constants;
 import es.storeapp.web.cookies.UserInfo;
 import java.beans.XMLDecoder;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Base64;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class AutoLoginInterceptor extends HandlerInterceptorAdapter {
 
@@ -33,14 +43,55 @@ public class AutoLoginInterceptor extends HandlerInterceptorAdapter {
             if (Constants.PERSISTENT_USER_COOKIE.equals(c.getName())) {
                 String cookieValue = c.getValue();
                 if (cookieValue == null) {
+                    // Si la cookie está vacía, ignoramos
                     continue;
                 }
-                Base64.Decoder decoder = Base64.getDecoder();
-                XMLDecoder xmlDecoder = new XMLDecoder(new ByteArrayInputStream(decoder.decode(cookieValue)));
-                UserInfo userInfo = (UserInfo) xmlDecoder.readObject();
-                User user = userService.findByEmail(userInfo.getEmail());
-                if (user != null && user.getPassword().equals(userInfo.getPassword())) {
-                    session.setAttribute(Constants.USER_SESSION, user);
+                try {
+                    // Decodificamos el valor de la Cookie
+                    byte[] userInfoXML = Base64.getDecoder().decode(cookieValue);
+                    // Para solventar la vulnerabilidad de deserialización insegura, primero parseamos
+                    // el XML con un parser DOM y comprobamos que la clase que viene es un UserInfo
+                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                    // Habilitamos procesado seguro para mitigar XXE
+                    dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                    DocumentBuilder db = dbf.newDocumentBuilder();
+                    // Parseamos el XML a un documento y normalizamos el documento
+                    Document doc = db.parse(new ByteArrayInputStream(userInfoXML));
+                    doc.getDocumentElement().normalize();
+                    // Comprobamos que el contenido del XML es realmente un objeto UserInfo
+                    if (!(doc.getDocumentElement().getNodeName().equals("java"))){
+                        // El nodo raíz debe ser un nodo "java". Si no lo es, ignoramos la cookie
+                        continue;
+                    }
+                    NodeList objectNodes = doc.getElementsByTagName("*");
+                    if (objectNodes.getLength() != 1){
+                        // Si no hay exactamente un solo objeto, la cookie es inválida. La ignoramos
+                        continue;
+                    }
+                    Node objectNode = objectNodes.item(0);
+                    if (!(objectNode.getNodeName().equals("object"))){
+                        // Si no es un elemento de tipo "object", ignoramos
+                        continue;
+                    }
+                    if (!(((Element) objectNode).getAttribute("class").equals("es.storeapp.web.cookies.UserInfo"))) {
+                        // Si la clase no es UserInfo, ignoramos la cookie
+                        continue;
+                    }
+                    // Finalmente, si llegamos aquí es que la cookie es un archivo XML que contiene un documento java
+                    // con un único elemento "object" de la clase "es.storeapp.web.cookies.UserInfo
+                    // Por lo tanto ahora podemos pasarle el documento al XMLDecoder para que instancie el objeto.
+                    XMLDecoder xmlDecoder = new XMLDecoder(new ByteArrayInputStream(userInfoXML));
+                    UserInfo userInfo = (UserInfo) xmlDecoder.readObject();
+                    User user = userService.findByEmail(userInfo.getEmail());
+                    if (user != null && user.getPassword().equals(userInfo.getPassword())) {
+                        session.setAttribute(Constants.USER_SESSION, user);
+                    }
+                } catch (IllegalArgumentException | SAXException | ClassCastException e){
+                    // IllegalArgumentException -> Base64 inválido
+                    // SAXException -> XML Inválido
+                    // ClassCastException -> XML Válido pero no tiene ningún Elemento
+                    // Si no se puede parsear correctamente, ignoramos la cookie y seguimos adelante
+                    continue;
                 }
             }
         }
