0100097933744d9a28b46c36f52d3819033347a5
SamuS25 <samuel.santos@udc.es>
2022-12-05 12:54:32 +0100

    [OAuth] Implementadas las funciones para autenticarse con el servidor de OAuth.

--- tasks-app/src/main/resources/static/application/frontend/users/OAuthLogin.js
+++ tasks-app/src/main/resources/static/application/frontend/users/OAuthLogin.js
@@ -1,16 +1,21 @@
 var OAuthLogin = (props) => {
     if(location.hash) {
         const hash = window.location.hash.substring(1);
-        const params = {};
+        const params =  new URLSearchParams(hash);
+        const handleParams = () => {
+            params.access_token = params.get('access_token');
+        };
+        handleParams();
         if(params.access_token) {
             var jwtToken = jwt.parseJwtToken(params.access_token);
-            jwt.storeJwtToken(params.access_token);
             props.dispatch({
                 type: 'login',
                 user: jwtToken.user_name,
                 authorities: jwtToken.authorities,
                 token: params.access_token
             });
+            alerts.success('Welcome ' + escapeHtml(jwtToken.sub) + '!');
+            jwt.storeJwtToken(params.access_token);
             return (<ReactRouterDOM.Redirect to="/"/>);
         }
     }
